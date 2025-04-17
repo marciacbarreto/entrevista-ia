7c4a1ca791aebe72be8880909cbaa6fee1eb2943
marciacbarreto <marciacbarreto@gmail.com>
2025-04-17 16:34:06 -0300

    interface_app_segura.py

--- interface_app_segura.py
+++ interface_app_segura.py
@@ -1,2 +1,12 @@
 # Atualização completa com Conecte-se, carregar e página 3
+import streamlit as st
+
+# Ocultar a barra superior do Streamlit
+st.markdown("""
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+    </style>
+""", unsafe_allow_html=True)
 
